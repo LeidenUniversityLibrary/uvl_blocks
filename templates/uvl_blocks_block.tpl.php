7bdfee9aa70cfc97b7c255f428d753a0fff7516b
Job Joling <job@jobjol.com>
2019-04-12 17:04:24 +0200

    Restore block template and add variable checks.

--- templates/uvl_blocks_block.tpl.php
+++ templates/uvl_blocks_block.tpl.php
@@ -5,6 +5,27 @@
  **/
 
 $items = $variables['items'];
+
+if (isset($items['button-more']) && isset($items['feat-browser'])): ?>
 ?>
+<div class="dc-feat-browser">
+  <div class="dc-feat-more">
+    <a href="<?php print $items['button-more']['link'] ?>">
+      <span>
+          <i class="fa fa-chevron-circle-right"></i>
+        <?php print t($items['button-more']['text']) ?>
+      </span>
+    </a>
+  </div>
+  <ul>
+    <?php
 
-
+    foreach ($items['feat-browser'] as $item) : ?>
+      <?php print render($item['image']); ?>
+      <?php print render($item['title']); ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+<?php
+endif;
+?>
